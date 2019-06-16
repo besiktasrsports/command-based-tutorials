cfa8311a00dc2064ad3fad588edc3ff6879c3c3a
Halil Ömer <halilomer03@gmail.com>
2019-06-16 11:36:55 +0300

    Initial Upload :fire:

---
+++ command-based-tutorial/deneme/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class DriveTrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  public RobotDrive robotDrive;
+  public DriveTrain(){
+    robotDrive = new RobotDrive(Robot.m_robotMap.driveTrainLeftFrontMotor,
+    Robot.m_robotMap.driveTrainLeftRearMotor, Robot.m_robotMap.driveTrainRightFrontMotor, Robot.m_robotMap.driveTrainRightRearMotor);
+      robotDrive.setSafetyEnabled(false);
+      robotDrive.setSensitivity(0.5);
+      robotDrive.setExpiration(0.1);
+      robotDrive.setMaxOutput(1.0);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+  public void driveBase(){
+    robotDrive.arcadeDrive(Robot.m_oi.xbox.getX(), Robot.m_oi.xbox.getY());
+  }
+  
+}
